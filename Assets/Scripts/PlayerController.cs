393af1abf58b52c20b22fef762579e05829bd7b5
MazurV2 <149969116+MazurV2@users.noreply.github.com>
2026-08-11 18:50:53 +0200

    Added Primitives and Movement
    - created a few materials
    - added primitives to the scene
    - created a new Input Actions asset
    - added a script for moving and tilting the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    private Controls controls;
+    private Vector2 currentMovement;
+
+    [SerializeField] private GameObject plane;
+
+    [SerializeField] private float baseMoveSpeed = 5f;
+    [SerializeField] private float baseTiltSpeed = 5f;
+
+    private float maxTiltX = 45f;
+    private float maxTiltY = 45f;
+
+    [SerializeField] private float xBoundary = 5f;
+    [SerializeField] private float yBoundary = 5f;
+
+    private void Awake()
+    {
+        controls = new Controls();
+    }
+
+    private void Update()
+    {
+        currentMovement = controls.Player.Move.ReadValue<Vector2>();
+        Move();
+        Tilt();
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Enable();   
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    private void Move()
+    {
+        transform.Translate(currentMovement * baseMoveSpeed * Time.deltaTime);
+        ClampPosition();
+    }
+
+    private void ClampPosition()
+    {
+        Vector3 newPosition = transform.position;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -xBoundary, xBoundary);
+        newPosition.y = Mathf.Clamp(newPosition.y, -yBoundary, yBoundary);
+
+        transform.position = newPosition;
+    }
+
+    private void Tilt()
+    {
+        float pitch = -currentMovement.y * maxTiltY;
+        float roll = -currentMovement.x * maxTiltX;
+
+        Quaternion targetRotation = Quaternion.Euler(pitch, 0f, roll);
+
+        plane.transform.rotation = Quaternion.Slerp(plane.transform.rotation, targetRotation, baseTiltSpeed * Time.deltaTime);
+    }
+}
