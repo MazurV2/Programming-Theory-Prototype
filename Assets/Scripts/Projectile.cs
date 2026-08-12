8e7927db4896483d621f5b110973224079e83305
MazurV2 <149969116+MazurV2@users.noreply.github.com>
2026-08-12 02:30:42 +0200

    Added Early Weapon System
    - added shooting mechanic with object pooling for the projectiles

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,6 +1,47 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Projectile : MonoBehaviour
 {
-    
+    private IObjectPool<GameObject> pool;
+    private bool isReleased;
+
+    [SerializeField] private float baseLifetime = 3f;
+    [SerializeField] private float baseSpeed = 5f;
+
+    private float currentLifetime;
+
+    public void SetPool(IObjectPool<GameObject> pool)
+    {
+        this.pool = pool;
+    }
+
+    private void OnEnable()
+    {
+        currentLifetime = baseLifetime;
+        isReleased = false;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * baseSpeed * Time.deltaTime);
+
+        currentLifetime -= Time.deltaTime;
+        if (currentLifetime <= 0f)
+        {
+            ReleaseToPool();
+        } 
+    }
+
+    private void ReleaseToPool()
+    {
+        if (pool != null && !isReleased)
+        {
+            isReleased = true;
+            pool.Release(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/WeaponSystem.cs
+++ Assets/Scripts/WeaponSystem.cs
@@ -1,6 +1,87 @@
+using System;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class WeaponSystem : MonoBehaviour
 {
+    [SerializeField] private InputReaderSO inputReader;
     
+    private bool isShooting;
+    [SerializeField] private float fireRate = 0.5f;
+    private float nextFireTime;
+
+    private IObjectPool<GameObject> projectilePool;
+    [SerializeField] private GameObject projectilePrefab;
+    private int basePoolSize = 18;
+    private int maxPoolSize = 36;
+
+    private void Awake()
+    {
+        projectilePool = new ObjectPool<GameObject>(
+            createFunc: CreateProjectile,
+            actionOnGet: OnGetFromPool,
+            actionOnRelease: OnReleaseToPool,
+            actionOnDestroy: OnDestroyObject,
+            true,
+            basePoolSize,
+            maxPoolSize);
+    }
+
+    private GameObject CreateProjectile()
+    {
+        GameObject gameObject = Instantiate(projectilePrefab);
+
+        if (gameObject.TryGetComponent<Projectile>(out var projectile))
+        {
+            projectile.SetPool(projectilePool);
+        }
+
+        return gameObject;
+    }
+
+    private void OnGetFromPool(GameObject projectile)
+    {
+        projectile.transform.position = transform.position;
+        projectile.transform.rotation = transform.rotation;
+        projectile.SetActive(true);
+    }
+
+    private void OnReleaseToPool(GameObject projectile)
+    {
+        projectile.SetActive(false);
+    }
+
+    private void OnDestroyObject(GameObject projectile)
+    {
+        Destroy(projectile);
+    }
+
+    private void OnEnable()
+    {
+        inputReader.onShootInput += ChangeShootingState;
+    }
+
+    private void OnDisable()
+    {
+        inputReader.onShootInput -= ChangeShootingState;
+    }
+
+    private void Update()
+    {
+        if (isShooting && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Shoot();
+        }
+    }
+
+    private void ChangeShootingState(bool shooting)
+    {
+        isShooting = shooting;
+    }
+
+    private void Shoot()
+    {
+        projectilePool.Get();
+    }
 }
